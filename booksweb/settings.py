3650d789387b16a12752bb471a0703ff1274fb11
omuya <simonotienoomuya@gmail.com>
2025-04-01 05:03:43 +0300

    render

--- booksweb/settings.py
+++ booksweb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here'  # Change this in production
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
